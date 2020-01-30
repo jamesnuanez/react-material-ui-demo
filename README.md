473cbe27cb06140ddc160dfcd192c82b8607df38
James Nuanez <james@jamesnuanez.com>
2020-01-29 23:45:08 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Material-UI Demo
+
+Built by [James Nuanez](https://www.jamesnuanez.com) in 2020.
+
+https://github.com/jamesnuanez/
+
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Material UI Demo</h1>
+    </div>
+  );
+}
+
+export default App;
