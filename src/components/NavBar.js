5dcd7746192925e5656f83f8888eaec4b9dcaace
James Nuanez <james@jamesnuanez.com>
2020-01-30 15:04:58 -0800

    File clean up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Route, Switch, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(theme => ({
   toolbar: {
